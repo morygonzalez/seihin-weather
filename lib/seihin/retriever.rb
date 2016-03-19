34fedcede2a32a007195ad2be2c6ed086ae6d102
森井ゴンザレス <morygonzalez@gmail.com>
2016-03-19 18:13:38 +0900

    Add feature to cache API response

--- lib/seihin/retriever.rb
+++ lib/seihin/retriever.rb
@@ -10,12 +10,42 @@ module Seihin
           raise AuthenticationError, 'Authentication Failed!'
         end
 
-        data   = @client.get_station_data
+        data = get_data
         result = format(data)
+        close_file
         puts result
       end
 
       private
+
+      def get_data
+        data = @client.get_station_data
+        store_cache(data)
+        data
+      rescue Faraday::ConnectionFailed
+        restore_cache
+      end
+
+      def cache_file
+        tmp_dir = File.expand_path(File.join(File.dirname(__FILE__), '../../tmp'))
+        Dir.mkdir(tmp_dir) unless Dir.exist?(tmp_dir)
+        cache_path = "#{tmp_dir}/cache"
+        @cache_file ||= File.open(cache_path, 'w')
+      end
+
+      def restore_cache
+        if test('m', cache_file) > Time.now - 15 * 60
+          cache_file.read
+        end
+      end
+
+      def store_cache(data)
+        cache_file.write(data)
+      end
+
+      def close_file
+        cache_file.close
+      end
 
       def format(data)
         formatter = Formatter.new(data)
